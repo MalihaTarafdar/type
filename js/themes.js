8a46c6f74fea039011b9b71dd47e2079715ee2a0
Maliha Tarafdar <malihatarafdar@yahoo.com>
2020-06-08 17:54:51 -0400

    Add themeing functionality and add modularity to themes

---
+++ js/themes.js
@@ -0,0 +1,23 @@
+export let themes = new Map()
+themes.set('nord', {
+	"--bg-color": "#242933",
+	"--text-color": "#ECEFF4",
+	"--terminal-color": "#2E3440",
+	"--input-color": "#3B4252",
+	"--redo-color": "#3B4252",
+	"--highlight-color": "#4d8ee9",
+	"--correct-color": "#A3BE8C",
+	"--incorrect-color": "#BF616A",
+	"--neutral-color": "#B38DAC"
+})
+themes.set('dark', {
+	"--bg-color": "#1A1A1A",
+	"--text-color": "#FFFFFF",
+	"--terminal-color": "#333333",
+	"--input-color": "#1A1A1A",
+	"--redo-color": "#1A1A1A",
+	"--highlight-color": "#4d8ee9",
+	"--correct-color": "#66B422",
+	"--incorrect-color": "#C6262E",
+	"--neutral-color": "#A56DE2"
+})
