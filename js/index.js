0b2e4a8b620a19fa04e4a5f891830463a993423a
Maliha Tarafdar <malihatarafdar@yahoo.com>
2020-06-12 21:10:48 -0400

    Add wordcount mode functionality

--- js/index.js
+++ js/index.js
@@ -2,18 +2,38 @@ import { setTheme } from './theme.js'
 import ShortcutHandler from './shortcuts.js'
 import WordCountMode from './wordcount.js'
 
-const input = document.querySelector('#terminal-input')
 const text = document.querySelector('#terminal > #text')
+const input = document.querySelector('#terminal > #input-container > #input')
 
 class _WordCountMode extends WordCountMode {
-	async setText() {
-		await super.setText()
+	constructor() {
+		super()
+		input.addEventListener('keypress', e => {
+			if (e.key === ' ') {
+				//delay to clear input
+				setTimeout(() => {
+					input.value = ''
+				}, 50);
+
+				let word = text.querySelector(`span:nth-child(${this.cur + 1})`)
+				word.classList.add((this.wordList[this.cur] === input.value) ? 'correct' : 'incorrect')
+				word.classList.remove('current')
+
+				this.cur++
+				word = text.querySelector(`span:nth-child(${this.cur + 1})`)
+				word.classList.add('current')
+			}
+		})
+	}
+	async setText(wordCount) {
+		await super.setText(wordCount)
 		text.innerHTML = ''
-		for (let i = 0; i < this.wordList.length; i++) {
+		for (let i = 0; i < wordCount; i++) {
 			let span = document.createElement('span')
 			span.textContent = this.wordList[i] + ' '
 			text.appendChild(span)
 		}
+		text.querySelector('#terminal > #text > span:first-child').classList.add('current')
 	}
 }
 
@@ -25,7 +45,7 @@ class _ShortcutHandler extends ShortcutHandler {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	new _WordCountMode().setText()
 	setTheme('nord')
+	new _WordCountMode().setText(10)
 	new _ShortcutHandler()
 })
--- js/wordcount.js
+++ js/wordcount.js
@@ -2,13 +2,12 @@ export default class WordCountMode {
 	constructor() {
 		this.WORDLIST_COUNT = 1
 		this.wordList = []
-		this.setWordList()
-		this.currentWordInd = 0
+		this.cur = 0
 	}
-	async setText() {
-		await this.setWordList()
+	async setText(wordCount) {
+		await this.setWordList(wordCount)
 	}
-	async setWordList() {
+	async setWordList(wordCount) {
 		const res = await fetch(`texts/words/list${Math.floor(Math.random() * this.WORDLIST_COUNT) + 1}.txt`)
 		if (!res.ok) return
 		const text = await res.text()
@@ -22,5 +21,6 @@ export default class WordCountMode {
 			}
 		}
 		this.wordList.sort(() => 0.5 - Math.random())
+		this.wordList.length = wordCount
 	}
 }
