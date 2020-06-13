734f3b53d82e304c3b6d1f541c14bb389f0d149a
Maliha Tarafdar <malihatarafdar@yahoo.com>
2020-06-12 21:20:24 -0400

    Refactor wordcount mode code into module

--- js/index.js
+++ js/index.js
@@ -5,38 +5,6 @@ import WordCountMode from './wordcount.js'
 const text = document.querySelector('#terminal > #text')
 const input = document.querySelector('#terminal > #input-container > #input')
 
-class _WordCountMode extends WordCountMode {
-	constructor() {
-		super()
-		input.addEventListener('keypress', e => {
-			if (e.key === ' ') {
-				//delay to clear input
-				setTimeout(() => {
-					input.value = ''
-				}, 50);
-
-				let word = text.querySelector(`span:nth-child(${this.cur + 1})`)
-				word.classList.add((this.wordList[this.cur] === input.value) ? 'correct' : 'incorrect')
-				word.classList.remove('current')
-
-				this.cur++
-				word = text.querySelector(`span:nth-child(${this.cur + 1})`)
-				word.classList.add('current')
-			}
-		})
-	}
-	async setText(wordCount) {
-		await super.setText(wordCount)
-		text.innerHTML = ''
-		for (let i = 0; i < wordCount; i++) {
-			let span = document.createElement('span')
-			span.textContent = this.wordList[i] + ' '
-			text.appendChild(span)
-		}
-		text.querySelector('#terminal > #text > span:first-child').classList.add('current')
-	}
-}
-
 class _ShortcutHandler extends ShortcutHandler {
 	onInputFocus() {
 		super.onInputFocus()
@@ -46,6 +14,6 @@ class _ShortcutHandler extends ShortcutHandler {
 
 document.addEventListener('DOMContentLoaded', () => {
 	setTheme('nord')
-	new _WordCountMode().setText(10)
+	new WordCountMode(input).setText(10, text)
 	new _ShortcutHandler()
 })
--- js/wordcount.js
+++ js/wordcount.js
@@ -1,12 +1,47 @@
 export default class WordCountMode {
-	constructor() {
+	constructor(input) {
 		this.WORDLIST_COUNT = 1
 		this.wordList = []
 		this.cur = 0
+		input.addEventListener('keypress', e => {
+			if (e.key === ' ') {
+				//delay to clear input
+				setTimeout(() => {
+					input.value = ''
+				}, 50);
+				this.check(text)
+				this.next(text)
+			}
+		})
 	}
-	async setText(wordCount) {
+
+	reset() {
+
+	}
+
+	check(text) {
+		const word = text.querySelector(`span:nth-child(${this.cur + 1})`)
+		word.classList.add((this.wordList[this.cur] === input.value) ? 'correct' : 'incorrect')
+		word.classList.remove('current')
+	}
+
+	next(text) {
+		this.cur++
+		const word = text.querySelector(`span:nth-child(${this.cur + 1})`)
+		word.classList.add('current')
+	}
+
+	async setText(wordCount, text) {
 		await this.setWordList(wordCount)
+		text.innerHTML = ''
+		for (let i = 0; i < wordCount; i++) {
+			let span = document.createElement('span')
+			span.textContent = this.wordList[i] + ' '
+			text.appendChild(span)
+		}
+		text.querySelector('#terminal > #text > span:first-child').classList.add('current')
 	}
+
 	async setWordList(wordCount) {
 		const res = await fetch(`texts/words/list${Math.floor(Math.random() * this.WORDLIST_COUNT) + 1}.txt`)
 		if (!res.ok) return
@@ -16,9 +51,7 @@ export default class WordCountMode {
 			if (text.charAt(i) === ' ') {
 				if (word != '') this.wordList.push(word)
 				word = ''
-			} else {
-				word += text.charAt(i)
-			}
+			} else word += text.charAt(i)
 		}
 		this.wordList.sort(() => 0.5 - Math.random())
 		this.wordList.length = wordCount
